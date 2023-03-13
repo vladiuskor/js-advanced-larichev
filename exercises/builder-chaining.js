6e0b34d2624fe01be3689dd5bfe3fe8ff07aea75
Vlad Korobko <max.vlad.kor@gmail.com>
2023-03-13 22:21:42 +0200

    Passed lesson about Builder and Chaining

---
+++ exercises/builder-chaining.js
@@ -0,0 +1,40 @@
+'use strict'
+
+class Wallet {
+    balance = 0;
+
+    add(sum) {
+        this.balance += sum;
+        return this;
+    }
+
+    remove(sum) {
+        this.balance -= sum;
+        return this;
+    }
+}
+
+const wallet = new Wallet();
+const res = wallet.add(100).remove(10).add(20);
+console.log(res);
+
+class Builder {
+    house = {};
+
+    addRoof() {
+        this.house.roof = 'Roof';
+        return this;
+    }
+
+    addFloor() {
+        this.house.floor = 'Floor';
+        return this;
+    }
+
+    execute() {
+        return this.house;
+    }
+}
+
+const res2 = new Builder().addRoof().addFloor().execute();
+console.log(res2);
