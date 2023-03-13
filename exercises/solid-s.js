3146a1ca834bda04805ab43112e404493a459d8f
Vlad Korobko <max.vlad.kor@gmail.com>
2023-03-13 22:29:04 +0200

    Passed lesson about SOLID first principe

---
+++ exercises/solid-s.js
@@ -0,0 +1,24 @@
+'use strict'
+
+class Character {
+    #inventory = [];
+    #health = 10;
+
+    pickItem(item) {
+        this.#inventory.push(item);
+    }
+
+    recieveDamage(damage) {
+        this.#health -= damage;
+    }
+}
+
+class DB {
+    save(item) {
+        localStorage.setItem('char', item);
+    }
+
+    loadCharacter() {
+        //////.....
+    }
+}
