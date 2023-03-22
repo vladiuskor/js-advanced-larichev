3cf2e2343f4d2f7d153f407f53af9f21355f1f8b
Vlad Korobko <max.vlad.kor@gmail.com>
2023-03-22 15:48:46 +0200

    Passed promise lessons about catch and finally

---
+++ exercises/promis-reject.js
@@ -0,0 +1,16 @@
+'use strict'
+
+
+fetch('https://dummyjson.com/products')
+    .then(response => response.json())
+    .then(({products}) => {
+        console.log(products);
+        return fetch('https://dummyjson.com/products/' + products[0].id);
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+    })
