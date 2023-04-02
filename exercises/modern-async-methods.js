4af03e7077a13a5778da950c6c252dd97e97ba61
Vlad Korobko <max.vlad.kor@gmail.com>
2023-04-02 22:23:27 +0300

    Passed lesson about async methods

---
+++ exercises/modern-async-methods.js
@@ -0,0 +1,18 @@
+'use strict'
+
+class ProductRepository {
+    async getProducts() {
+        const response = await fetch('https://dummyjson.com/products');
+        console.log(await response.json())
+    }
+}
+
+const repo = new ProductRepository();
+repo.getProducts();
+
+const asyncArrow = async () => {
+    const response = await fetch('https://dummyjson.com/products');
+    console.log(await response.json())
+}
+
+asyncArrow();
