0a5860409790a273a7da3554324bc755d652ede1
Vlad Korobko <max.vlad.kor@gmail.com>
2023-04-10 22:11:56 +0300

    Passed lesson about lifting

--- exercises/dom-event-lifting.js
+++ exercises/dom-event-lifting.js
@@ -1,2 +1,30 @@
 'use strict'
 
+const button = document.querySelector('.button');
+const inner = document.querySelector('.inner');
+const wrapper = document.querySelector('.wrapper');
+
+button.addEventListener('click', function(event) {
+    console.log('Button');
+    console.log(event.target);
+    console.log(event.currentTarget);
+    this.style.backgroundColor = 'purple';
+});
+
+inner.addEventListener('click', function(event) {
+    console.log('Inner');
+    console.log(event.target);
+    console.log(event.currentTarget);
+    this.style.backgroundColor = 'blue';
+    event.stopPropagation();
+});
+
+
+wrapper.addEventListener('click', function(event) {
+    console.log('Wrapper');
+    console.log(event.target);
+    console.log(event.currentTarget);
+    this.style.backgroundColor = 'green';
+}, true);
+
+
