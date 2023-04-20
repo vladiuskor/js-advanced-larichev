0935b309c61111b05a1027a5ed96d5c0fd2580d3
Vlad Korobko <max.vlad.kor@gmail.com>
2023-04-20 17:54:18 +0300

    Wrote header with styles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import { DivComponent } from "../../common/div-component";
+import './header.css';
 
 export class Header extends DivComponent {
     constructor(appState) {
@@ -12,6 +13,20 @@ export class Header extends DivComponent {
         this.el.innerHTML = `
             <div>
                 <img src="/static/logo.svg" alt="логотип" />
+            </div>
+            <div class="menu">
+                <a class="menu__item" href="#">
+                    <img src="/static/search.svg" alt="Пошук іконка" />
+                    Пошук книг
+                </a>
+                <a class="menu__item" href="#favorites">
+                    <img src="/static/favorites.svg" alt="Обране іконка" />
+                    Обране
+
+                    <div class="menu__counter">
+                        ${this.appState.favorites.length}
+                    </div>
+                </a>
             </div>
         `;
 
