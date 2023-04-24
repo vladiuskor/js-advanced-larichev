ff5ce53ba25b7dfaaf069e3f5055c00661a80f9f
Vlad Korobko <max.vlad.kor@gmail.com>
2023-04-24 21:25:35 +0300

    Added card layout

--- src/components/card-list/card-list.js
+++ src/components/card-list/card-list.js
@@ -1,5 +1,6 @@
 import { DivComponent } from "../../common/div-component";
 import './card-list.css';
+import { Card } from "../card/card";
 
 export class CardList extends DivComponent {
     constructor(appState, parentState) {
@@ -20,8 +21,11 @@ export class CardList extends DivComponent {
 
         this.el.classList.add('card_list');
         this.el.innerHTML = `
-            <h1>Знайдено книг - ${this.parentState.list.length}</h1>
+            <h1>Знайдено книг - ${this.parentState.numFound}</h1>
         `
+        for (const card of this.parentState.list) {
+            this.el.append(new Card(this.appState, card).render())
+        }
         return this.el;
     }
 }
