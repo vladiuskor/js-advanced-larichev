a8da8e08b3f8dbce5d2ce881dabce4f4298f6233
Vlad Korobko <max.vlad.kor@gmail.com>
2023-04-24 16:11:06 +0300

    Added loading state and showing result after request

---
+++ src/components/card-list/card-list.js
@@ -0,0 +1,27 @@
+import { DivComponent } from "../../common/div-component";
+import './card-list.css';
+
+export class CardList extends DivComponent {
+    constructor(appState, parentState) {
+        super();
+        this.appState = appState;
+        this.parentState = parentState;
+    }
+
+    render() {
+        if(this.parentState.loading) {
+            this.el.innerHTML = `
+                <div class="card_list__loader">
+                    Завантаження...
+                </div>
+            `;
+            return this.el;
+        }
+
+        this.el.classList.add('card_list');
+        this.el.innerHTML = `
+            <h1>Знайдено книг - ${this.parentState.list.length}</h1>
+        `
+        return this.el;
+    }
+}
--- src/views/main/main.js
+++ src/views/main/main.js
@@ -2,6 +2,7 @@ import { AbstractView } from "../../common/view";
 import onChange from "on-change";
 import { Header } from "../../components/header/header";
 import { Search } from "../../components/search/search";
+import { CardList } from "../../components/card-list/card-list";
 
 export class MainView extends AbstractView {
     state = {
@@ -32,6 +33,14 @@ export class MainView extends AbstractView {
             this.state.loading = false;
             this.state.list = data.docs;
         }
+
+        if(path === 'loading') {
+            this.render();
+        }
+
+        if(path === 'list') {
+            this.render();
+        }
     }
 
     async loadList(q, offset) {
@@ -41,7 +50,8 @@ export class MainView extends AbstractView {
 
     render() {
         const main = document.createElement('div');
-        main.append(new Search(this.state).render())
+        main.append(new Search(this.state).render());
+        main.append(new CardList(this.appState, this.state).render());
         this.app.innerHTML = '';
         this.app.append(main);
         this.renderHeader();
